ab2b8b81d5cab7c4c2342aad91687a71f718bad4
c0664589 <c0664589@SB-N208-01.lambton.on.ca>
2015-09-15 11:46:18 -0400

    Initial Commit

---
+++ src/javaassignment/pkg2015/JavaAssignment2015.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassignment.pkg2015;
+
+/**
+ *
+ * @author c0664589
+ */
+public class JavaAssignment2015 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*JOB Title:-Java Developer
+
+Employer-TATA Consultancy Services-TCS-TORONTO, ON
+
+Requirements:
+
+ 1)PERL:- High-level,Interpretd & Dynamic Programming Language.
+
+   Link:-https://en.wikipedia.org/wiki/Perl
+
+
+ 2)PYTHON:- General-purpose,High-level language.
+
+   Link:-https://en.wikipedia.org/wiki/Python_(programming_language)
+
+
+ 3)SHELL:- Scripting Unix-like Systems.
+
+   Link:-https://en.wikipedia.org/wiki/Shell_script
+
+
+Salary:
+
+ 1)Toronto:Annual-$71k
+
+ 2)BC:Annual-$69k
+
+ 3)WATERLOO-$73K
+                */
+    }
+    
+}
